03001c1a1ae7e67b51a1fad0fa7dcf394c07432c
Ashvani Pandey <ashvanipandey51@gmail.com>
2025-09-07 00:51:34 +0530

    add vm code

--- module/dev/main.tf
+++ module/dev/main.tf
@@ -1,3 +1,24 @@
 module "rg" {
   source = "../../Resource_Group"
+  name = "rg123"
+}
+
+module "vnet" {
+  depends_on = [ module.rg ]
+  source = "../../Virtual_Network"
+  name = "vnet001"
+  location = "westus2"
+}
+
+module "subnet" {
+  depends_on = [ module.vnet ]
+  source = "../../subnet"
+  name = "subnet001"
+  address_prefixes = [ "10.0.0.0/24" ]
+  vnet = "vnet001"
+  rg="rg123"
+}
+module "vm" {
+    depends_on = [ module.subnet ]
+  source = "../../virtual_machine"
 }
