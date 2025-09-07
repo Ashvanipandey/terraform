55db81f0c05b8e78b120616542fe78692b71e622
Ashvani Pandey <ashvanipandey51@gmail.com>
2025-09-07 15:22:02 +0530

    add nsg nic

--- module/dev/main.tf
+++ module/dev/main.tf
@@ -1,6 +1,6 @@
 module "rg" {
   source = "../../Resource_Group"
-  name = "rg123"
+  name = "rg121"
 }
 
 module "vnet" {
@@ -8,7 +8,7 @@ module "vnet" {
   source = "../../Virtual_Network"
   name = "vnet001"
   location = "westus2"
-  rg="rg123"
+  rg="rg121"
 }
 
 module "subnet" {
@@ -17,13 +17,27 @@ module "subnet" {
   name = "subnet001"
   address_prefixes = [ "10.0.0.0/24" ]
   vnet = "vnet001"
-  rg="rg123"
+  rg="rg121"
 }
 module "vm" {
-    depends_on = [ module.subnet ]
+    depends_on = [ module.subnet,module.nic ]
   source = "../../virtual_machine"
+  nicname = "my-nic"
 }
 module "lb" {
     depends_on = [ module.vm ]
   source = "../../loadbalancer"
+  rg="rg121"
+  nsgname = "nsg001"
+}
+module "nic" {
+    depends_on = [ module.subnet ]
+  source = "../../nic"
+}
+module "nsg" {
+    depends_on = [ module.rg ]
+  source = "../../nsg"
+  name = "nsg001"
+  rg="rg121"
+  location = "westus2"
 }
