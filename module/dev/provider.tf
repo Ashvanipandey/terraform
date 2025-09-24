80dcb2274d6917e417f471651def6afd25284a84
Ashvani Pandey <ashvanipandey51@gmail.com>
2025-09-24 07:24:49 +0530

    add backed bloc

--- module/dev/provider.tf
+++ module/dev/provider.tf
@@ -1,11 +1,19 @@
 terraform {
+ required_version = ">=1.3.0"
   required_providers {
     azurerm={
         source = "hashicorp/azurerm"
         version = "~>3.0"
     }
+    
   }
- required_version = ">=1.3.0"
+ backend "azurerm" {
+   resource_group_name = "rg111"
+   storage_account_name = "strglnp"
+   container_name = "container"
+   key = "terraform.tfstate"
+ }
+
 }
 provider "azurerm" {
   features {
